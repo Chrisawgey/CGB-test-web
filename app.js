8582f45d0576d6a0951660a67ecf5bf578ecc1cf
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-02-09 23:36:08 +0000

    EDITZ

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+let navbar__toggle = document.querySelector(".navbar__toggle");
+let navbar__menu = document.querySelector(".navbar__menu");
 
 menu.addEventListener('click',  function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.onclick= function(e){
+    if(!navbar__toggle.contains(e.target) && !navbar__menu.contains(e.target) ) {
+    navbar__toggle.classList.remove("is-active");
+    navbar__menu.classList.remove("active");
+    m
+     }
+}
