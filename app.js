4ca5b36f8177e81d87c53e905ffce0ad48c84171
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-02-10 01:37:31 +0000

    edit

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ menu.addEventListener('click',  function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+$(document).ready(function(){
+    $('.navbar__container').click(function(event){
+        event.stopPropagation();
+        $(".navbar__toggle").slideToggle("fast");
+    });
+    $(".navbar_toggle").on(".navbar__container", function (event) {
+        event.stopPropagation();
+    });
+});
+
+$(document).on("click", function() {
+    $(".showup").hide();
+});
